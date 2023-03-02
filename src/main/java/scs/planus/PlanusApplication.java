0fa17f25876c4dcb7a340c93b42a0b9da9e387ed
leesangjin <tkdwls0628@gmail.com>
2023-03-02 16:14:17 +0900

    Initial commit

---
+++ src/main/java/scs/planus/PlanusApplication.java
@@ -0,0 +1,13 @@
+package scs.planus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanusApplication.class, args);
+	}
+
+}
