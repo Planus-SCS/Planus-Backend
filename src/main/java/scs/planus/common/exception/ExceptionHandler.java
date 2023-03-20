ed111e78b72cf58ed52e40947494d501fe957e96
leesangjin <tkdwls0628@gmail.com>
2023-03-20 18:42:24 +0900

    [Rename]: 클래스명 변경

--- src/main/java/scs/planus/common/exception/ExceptionHandler.java
+++ src/main/java/scs/planus/common/exception/ExceptionHandler.java
@@ -1,15 +1,14 @@
 package scs.planus.common.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import scs.planus.common.response.BaseResponse;
 import scs.planus.common.response.ResponseStatus;
 
 @RestControllerAdvice
-public class PlanusExceptionHandler {
+public class ExceptionHandler {
 
-    @ExceptionHandler(PlanusException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(PlanusException.class)
     public ResponseEntity<Object> handlePlanusException(PlanusException e) {
         ResponseStatus status = e.getStatus();
         return ResponseEntity.status(status.getHttpStatus())
