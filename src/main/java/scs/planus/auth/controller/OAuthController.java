d331ae9b9f6b400c62b61d6e56f7d78edb845276
leesangjin <tkdwls0628@gmail.com>
2023-03-22 21:24:04 +0900

    [Feat]: Code Grant 방식의 소셜 로그인 컨트롤러 구현

---
+++ src/main/java/scs/planus/auth/controller/OAuthController.java
@@ -0,0 +1,26 @@
+package scs.planus.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import scs.planus.auth.dto.OAuthLoginResponseDto;
+import scs.planus.auth.service.OAuthService;
+import scs.planus.common.response.BaseResponse;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class OAuthController {
+
+    private final OAuthService oAuthService;
+
+    @GetMapping("/oauth2/{socialType}")
+    public BaseResponse socialLogin(@PathVariable String socialType,
+                                    @RequestParam String code) {
+        OAuthLoginResponseDto loginResponseDto = oAuthService.login(socialType, code);
+        return new BaseResponse(loginResponseDto);
+    }
+}
