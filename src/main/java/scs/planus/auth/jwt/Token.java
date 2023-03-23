56b1503b99ffd15820cc74d14058efe61d0f8d12
leesangjin <tkdwls0628@gmail.com>
2023-03-23 20:44:02 +0900

    [Feat]: accessToken과 refreshToken을 필드로 가지는 Token 객체 구현

---
+++ src/main/java/scs/planus/auth/jwt/Token.java
@@ -0,0 +1,12 @@
+package scs.planus.auth.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Token {
+
+    private String accessToken;
+    private String refreshToken;
+}
