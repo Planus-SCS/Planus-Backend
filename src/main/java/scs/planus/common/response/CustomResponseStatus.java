9a492017c2cadf85e9871cbb1729590521391be2
leesangjin <tkdwls0628@gmail.com>
2023-03-26 18:22:13 +0900

    [Feat]: jwt 관련 커스텀 예외 추가

--- src/main/java/scs/planus/common/response/CustomResponseStatus.java
+++ src/main/java/scs/planus/common/response/CustomResponseStatus.java
@@ -4,16 +4,20 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 @RequiredArgsConstructor
 public enum CustomResponseStatus implements ResponseStatus {
 
-    DUPLICATED_EMAIL(HttpStatus.CONFLICT, 2100, "중복된 이메일이 존재합니다."),
-    NONE_USER(HttpStatus.BAD_REQUEST, 2110, "존재하지 않는 회원입니다."),
-    NONE_SOCIAL_TYPE(HttpStatus.BAD_REQUEST, 2200, "존재하지 않는 소셜 로그인 타입입니다."),
+    DUPLICATED_EMAIL(CONFLICT, 2100, "중복된 이메일이 존재합니다."),
+    NONE_USER(BAD_REQUEST, 2110, "존재하지 않는 회원입니다."),
+    NONE_SOCIAL_TYPE(BAD_REQUEST, 2200, "존재하지 않는 소셜 로그인 타입입니다."),
 
     // jwt exception
-    UNAUTHORIZED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 2300, "인증되지 않은 토큰입니다.");
+    UNAUTHORIZED_ACCESS_TOKEN(UNAUTHORIZED, 2300, "인증되지 않거나 만료된 토큰입니다."),
+    FORBIDDEN_ACCESS_TOKEN(FORBIDDEN, 2301, "권한이 없는 토큰입니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final int code;
