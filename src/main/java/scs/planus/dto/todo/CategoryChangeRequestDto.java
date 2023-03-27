157f7c1742b6144e2970159ba9985fe1c8d04371
lcj1204 <dlckdwo1204@naver.com>
2023-03-27 14:54:39 +0900

    [Feat]: 변경 요청을 위한 CateogryChangeRequestDto 구현

---
+++ src/main/java/scs/planus/dto/todo/CategoryChangeRequestDto.java
@@ -0,0 +1,29 @@
+package scs.planus.dto.todo;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import scs.planus.domain.Color;
+import scs.planus.domain.TodoCategory;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryChangeRequestDto {
+    private String name;
+    private Color color;
+
+    @Builder
+    public CategoryChangeRequestDto(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public TodoCategory toEntity() {
+        return TodoCategory.builder()
+                .name(this.name)
+                .color(this.color)
+                .build();
+    }
+
+}
