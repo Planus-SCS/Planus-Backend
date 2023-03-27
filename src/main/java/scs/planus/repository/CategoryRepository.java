4692ca2a222e431e7cdd9b8d132087656390ce55
lcj1204 <dlckdwo1204@naver.com>
2023-03-27 14:48:12 +0900

    [Feat]: CategoryRepository 구현

---
+++ src/main/java/scs/planus/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package scs.planus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import scs.planus.domain.TodoCategory;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<TodoCategory, Long> {
+}
