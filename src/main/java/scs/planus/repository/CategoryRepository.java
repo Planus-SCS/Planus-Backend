6244490ea3389b6e1155092da3330f7496224ceb
lcj1204 <dlckdwo1204@naver.com>
2023-03-27 18:21:51 +0900

    [Feat]: @Repository 어노테이션 삭제

--- src/main/java/scs/planus/repository/CategoryRepository.java
+++ src/main/java/scs/planus/repository/CategoryRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import scs.planus.domain.TodoCategory;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<TodoCategory, Long> {
 }
