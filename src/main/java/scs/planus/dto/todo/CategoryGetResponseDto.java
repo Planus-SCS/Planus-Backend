1d976a864ca3d741d6f33a196cd7b90ee0f839ff
lcj1204 <dlckdwo1204@naver.com>
2023-03-31 04:25:55 +0900

    [Feat]: Category 전체 조회 ResponseDto 구현

---
+++ src/main/java/scs/planus/dto/todo/CategoryGetResponseDto.java
@@ -0,0 +1,22 @@
+package scs.planus.dto.todo;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import scs.planus.domain.Color;
+import scs.planus.domain.TodoCategory;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryGetResponseDto {
+    private Long id;
+    private String name;
+    private Color color;
+
+    public CategoryGetResponseDto(TodoCategory todoCategory) {
+        this.id = todoCategory.getId();
+        this.name = todoCategory.getName();
+        this.color = todoCategory.getColor();
+    }
+}
