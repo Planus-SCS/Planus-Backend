76201757931d90afb196def8e4df3f0a71f06f80
leesangjin <tkdwls0628@gmail.com>
2023-03-31 21:42:34 +0900

    [Feat]: 회원 정보 수정 비즈니스 로직 구현

--- src/main/java/scs/planus/domain/Member.java
+++ src/main/java/scs/planus/domain/Member.java
@@ -61,4 +61,10 @@ public class Member extends BaseTimeEntity{
         this.status = status;
         this.role = role;
     }
+
+    public void updateProfile(String nickname, String description, String profileImageUrl) {
+        this.nickname = nickname;
+        this.description = description;
+        this.profileImageUrl = profileImageUrl;
+    }
 }
