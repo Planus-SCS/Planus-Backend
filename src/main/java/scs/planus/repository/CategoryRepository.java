29cce2b81a61f9977790a1a0cc95d92eabc371cb
lcj1204 <dlckdwo1204@naver.com>
2023-04-01 17:53:18 +0900

    [Feat]: 전체조회를 위한 findAllByStatus() 구현

--- src/main/java/scs/planus/repository/CategoryRepository.java
+++ src/main/java/scs/planus/repository/CategoryRepository.java
@@ -1,8 +1,16 @@
 package scs.planus.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import scs.planus.domain.Member;
+import scs.planus.domain.Status;
 import scs.planus.domain.TodoCategory;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<TodoCategory, Long> {
+    @Query("select c from TodoCategory c where c.status= :status and c.member= :member")
+    List<TodoCategory> findAllByStatus(@Param("status") Status status, @Param("member") Member member);
 }
