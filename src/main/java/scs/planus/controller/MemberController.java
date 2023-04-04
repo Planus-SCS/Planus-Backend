933aa41b477791daaf1f380a466c6951d7c60d49
leesangjin <tkdwls0628@gmail.com>
2023-04-04 17:45:49 +0900

    [Feat]: 회원 탈퇴(Soft-deleting) API 구현

--- src/main/java/scs/planus/controller/MemberController.java
+++ src/main/java/scs/planus/controller/MemberController.java
@@ -3,6 +3,7 @@ package scs.planus.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,13 @@ public class MemberController {
                                                          @Valid @RequestPart(value = "updateRequestDto") MemberUpdateRequestDto memberUpdateRequestDto) {
         Long memberId = principalDetails.getId();
         MemberResponseDto responseDto = memberService.update(memberId, multipartFile, memberUpdateRequestDto);
+        return new BaseResponse<>(responseDto);
+    }
+
+    @DeleteMapping("/members")
+    public BaseResponse<MemberResponseDto> delete(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long memberId = principalDetails.getId();
+        MemberResponseDto responseDto = memberService.delete(memberId);
         return new BaseResponse<>(responseDto);
     }
 }
