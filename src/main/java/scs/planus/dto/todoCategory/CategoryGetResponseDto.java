938474731d276b1fd0002c0cf7b30730faecdd84
lcj1204 <dlckdwo1204@naver.com>
2023-04-04 15:00:34 +0900

    [Refactor]: 생성자 빌더 타입으로 수정

--- src/main/java/scs/planus/dto/todoCategory/CategoryGetResponseDto.java
+++ src/main/java/scs/planus/dto/todoCategory/CategoryGetResponseDto.java
@@ -1,21 +1,24 @@
 package scs.planus.dto.todoCategory;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import scs.planus.domain.Color;
 import scs.planus.domain.TodoCategory;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class CategoryGetResponseDto {
     private Long id;
     private String name;
     private Color color;
 
-    public CategoryGetResponseDto(TodoCategory todoCategory) {
-        this.id = todoCategory.getId();
-        this.name = todoCategory.getName();
-        this.color = todoCategory.getColor();
+    public static CategoryGetResponseDto of(TodoCategory todoCategory) {
+        return CategoryGetResponseDto.builder()
+                .id(todoCategory.getId())
+                .name(todoCategory.getName())
+                .color(todoCategory.getColor())
+                .build();
     }
 }
