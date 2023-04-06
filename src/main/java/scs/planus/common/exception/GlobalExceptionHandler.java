528b9db0d9b2a8c6e5a8142694fe1c12750abd8a
leesangjin <tkdwls0628@gmail.com>
2023-04-06 20:01:48 +0900

    [Feat]: Json 포맷팅 처리 ExceptionHandler 추가

--- src/main/java/scs/planus/common/exception/GlobalExceptionHandler.java
+++ src/main/java/scs/planus/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package scs.planus.common.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +21,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleValidException() {
+        ResponseStatus status = CommonResponseStatus.INVALID_PARAMETER;
+        return ResponseEntity.status(status.getHttpStatus())
+                .body(new BaseResponse<>(status));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleJsonException() {
         ResponseStatus status = CommonResponseStatus.INVALID_PARAMETER;
         return ResponseEntity.status(status.getHttpStatus())
                 .body(new BaseResponse<>(status));
