a53195f9a4ae3c47139af0d10fcf9b79791d718f
leesangjin <tkdwls0628@gmail.com>
2023-04-06 20:01:18 +0900

    [Rename]: ExceptionHandler 클래스명 변경

--- src/main/java/scs/planus/common/exception/GlobalExceptionHandler.java
+++ src/main/java/scs/planus/common/exception/GlobalExceptionHandler.java
@@ -1,30 +1,24 @@
 package scs.planus.common.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import scs.planus.common.response.BaseResponse;
 import scs.planus.common.response.CommonResponseStatus;
 import scs.planus.common.response.ResponseStatus;
 
-import org.springframework.validation.BindingResult;
-
-import java.util.List;
-
 @RestControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(PlanusException.class)
+    @ExceptionHandler(PlanusException.class)
     public ResponseEntity<Object> handlePlanusException(PlanusException e) {
         ResponseStatus status = e.getStatus();
         return ResponseEntity.status(status.getHttpStatus())
                 .body(new BaseResponse<>(status));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleValidException() {
         ResponseStatus status = CommonResponseStatus.INVALID_PARAMETER;
         return ResponseEntity.status(status.getHttpStatus())
