80338c79dea4cc82ac1a7401135c7007ca9541e5
leesangjin <tkdwls0628@gmail.com>
2023-04-10 18:55:03 +0900

    [Feat]: Todo id와 Member id를 이용한 현재 사용자의 투두 조회 쿼리 추가

--- src/main/java/scs/planus/repository/TodoRepository.java
+++ src/main/java/scs/planus/repository/TodoRepository.java
@@ -3,5 +3,9 @@ package scs.planus.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import scs.planus.domain.todo.Todo;
 
+import java.util.Optional;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    Optional<Todo> findByIdAndMemberId(Long id, Long memberId);
 }
