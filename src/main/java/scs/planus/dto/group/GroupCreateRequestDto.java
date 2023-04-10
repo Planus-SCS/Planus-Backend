8ef926859caff4775393941930522d51969ff88e
lcj1204 <dlckdwo1204@naver.com>
2023-04-10 16:07:16 +0900

    [Feat]: TagCreateRequestDto 를 리스트로 갖는 List<> 필드 추가(List가 가질 수 있는 Tag 갯수는 최대 5개로 제한)

--- src/main/java/scs/planus/dto/group/GroupCreateRequestDto.java
+++ src/main/java/scs/planus/dto/group/GroupCreateRequestDto.java
@@ -1,18 +1,24 @@
 package scs.planus.dto.group;
 
-import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.validator.constraints.URL;
-import scs.planus.domain.Group;
+import scs.planus.dto.tag.TagCreateRequestDto;
 
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import java.util.List;
 
 @Getter
 public class GroupCreateRequestDto {
+
     @NotBlank(message = "[request] 그룹명을 입력해 주세요.")
     @Size(min = 2, max = 20, message = "[request] 그룹명은 2 - 20글자로 입력해 주세요.")
     private String name;
+
     private String notice;
+
+    @Size(max = 5, message = "[request] 테그는 5개 이하로 입력해 주세요.")
+    private List<@Valid TagCreateRequestDto> tagList;
+
     @NotNull(message = "[request] 그룹 제한 인원을 설정해 주세요.")
     @Max(value = 50, message = "[request] 그룹 제한 인원은 50명 이하로 입력해 주세요.")
     @Min(value = 2, message = "[request] 그룹 제한 인원은 2명 이상으로 입력해 주세요.")
