0797a70ce9bf80d033bf771adf5b131e46c1aadc
leesangjin <tkdwls0628@gmail.com>
2023-04-10 19:00:33 +0900

    [Feat]: 일별 Todo 조회를 위한 조회 쿼리 추가

--- src/main/java/scs/planus/repository/TodoRepository.java
+++ src/main/java/scs/planus/repository/TodoRepository.java
@@ -1,11 +1,21 @@
 package scs.planus.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import scs.planus.domain.todo.Todo;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     Optional<Todo> findByIdAndMemberId(Long id, Long memberId);
+
+    @Query("select t from Todo t " +
+            "join fetch t.member m " +
+            "where m.id = :memberId and (t.startDate <= :date and t.endDate >= :date)")
+    List<Todo> findAllByMemberIdAndDate(@Param("memberId") Long memberId,
+                                        @Param("date") LocalDate date);
 }
