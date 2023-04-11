e9f2dc749eefe82122dc4cc2879327c9b8f6aebe
leesangjin <tkdwls0628@gmail.com>
2023-04-11 18:02:06 +0900

    [Refactor]: TodoRepository 패키지 계층 변경

--- src/main/java/scs/planus/repository/todo/TodoRepository.java
+++ src/main/java/scs/planus/repository/todo/TodoRepository.java
@@ -1,4 +1,4 @@
-package scs.planus.repository;
+package scs.planus.repository.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
