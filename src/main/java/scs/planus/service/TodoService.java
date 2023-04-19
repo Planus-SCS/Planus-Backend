aebb9dee1d55eeb3aa9b8ae2ee097676fbb03e08
leesangjin <tkdwls0628@gmail.com>
2023-04-19 14:56:02 +0900

    [Feat]: Todo 변경 기능 구현

--- src/main/java/scs/planus/service/TodoService.java
+++ src/main/java/scs/planus/service/TodoService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import scs.planus.common.exception.PlanusException;
+import scs.planus.domain.Group;
 import scs.planus.domain.Member;
 import scs.planus.domain.TodoCategory;
 import scs.planus.domain.todo.Todo;
@@ -13,6 +14,7 @@ import scs.planus.dto.todo.TodoDailyResponseDto;
 import scs.planus.dto.todo.TodoGetResponseDto;
 import scs.planus.dto.todo.TodoResponseDto;
 import scs.planus.repository.CategoryRepository;
+import scs.planus.repository.GroupRepository;
 import scs.planus.repository.MemberRepository;
 import scs.planus.repository.todo.TodoQueryRepository;
 import scs.planus.repository.todo.TodoRepository;
@@ -31,6 +33,7 @@ public class TodoService {
 
     private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
+    private final GroupRepository groupRepository;
     private final TodoRepository todoRepository;
     private final TodoQueryRepository todoQueryRepository;
 
@@ -71,6 +74,26 @@ public class TodoService {
     }
 
     @Transactional
+    public TodoGetResponseDto updateTodo(Long memberId, Long todoId, TodoCreateRequestDto requestDto) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new PlanusException(NONE_USER));
+
+        Todo todo = todoQueryRepository.findOneTodoById(todoId, member.getId())
+                .orElseThrow(() -> new PlanusException(NONE_TODO));
+
+        TodoCategory todoCategory = categoryRepository.findById(requestDto.getCategoryId())
+                .orElseThrow(() -> new PlanusException(NOT_EXIST_CATEGORY));
+
+        Group group = getGroup(requestDto.getGroupId());
+
+        validateDate(requestDto.getStartDate(), requestDto.getEndDate());
+        todo.update(requestDto.getTitle(), requestDto.getDescription(), requestDto.getStartTime(),
+                requestDto.getStartDate(), requestDto.getEndDate(), todoCategory, group);
+
+        return TodoGetResponseDto.of(todo);
+    }
+
+    @Transactional
     public TodoResponseDto deleteTodo(Long memberId, Long todoId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new PlanusException(NONE_USER));
@@ -88,5 +111,14 @@ public class TodoService {
                 throw new PlanusException(INVALID_DATE);
             }
         }
+    }
+
+    private Group getGroup(Long groupId) {
+        if (groupId == null) {
+            return null;
+        }
+        // TODO : 그룹 가입 이후, 현재 멤버가 해당 그룹에 가입했는지를 체크해야함. 아래 방식은 가입하지 않더라도 그룹 지정 가능한 방식
+        return groupRepository.findById(groupId)
+                    .orElseThrow(() -> new PlanusException(NOT_EXIST_CATEGORY));
     }
 }
