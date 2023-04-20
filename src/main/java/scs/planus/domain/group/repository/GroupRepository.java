135cd1cdf2857b4b87426d5a044795ab9b0cadb4
lcj1204 <dlckdwo1204@naver.com>
2023-04-20 17:51:48 +0900

    [Refactor]: groupMember fetch join 구현

--- src/main/java/scs/planus/domain/group/repository/GroupRepository.java
+++ src/main/java/scs/planus/domain/group/repository/GroupRepository.java
@@ -1,7 +1,13 @@
 package scs.planus.domain.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import scs.planus.domain.group.entity.Group;
 
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    @Query("select g from Group g join fetch g.groupMembers gm where g.id= :groupId")
+    Optional<Group> findWithGroupMemberById(@Param("groupId") Long aLong);
 }
