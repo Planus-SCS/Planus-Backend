e2197b587066c1753f7a28c7012a7970d07dd0e6
leesangjin <tkdwls0628@gmail.com>
2023-04-24 20:12:58 +0900

    [Feat]: 일별 Todo 중, 시간을 설정하지 않은 Todo 만을 위한 DTO 구현

---
+++ src/main/java/scs/planus/domain/todo/dto/TodoDailyDto.java
@@ -0,0 +1,29 @@
+package scs.planus.domain.todo.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import scs.planus.domain.todo.entity.Todo;
+
+@Getter
+@Builder
+public class TodoDailyDto {
+
+    private Long todoId;
+    private String title;
+
+    private Boolean isGroupMemberTodo;
+    private Boolean isPeriodTodo;
+    private Boolean hasDescription;
+    private Boolean isCompleted;
+
+    public static TodoDailyDto of(Todo todo) {
+        return TodoDailyDto.builder()
+                .todoId(todo.getId())
+                .title(todo.getTitle())
+                .isGroupMemberTodo(todo.getGroup() != null)
+                .isPeriodTodo(todo.getEndDate().isAfter(todo.getStartDate()))
+                .hasDescription(todo.getDescription() != null)
+                .isCompleted(todo.isCompletion())
+                .build();
+    }
+}
