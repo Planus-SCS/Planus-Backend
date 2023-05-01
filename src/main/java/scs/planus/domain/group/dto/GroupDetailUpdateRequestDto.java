f277a8670810c7b5187766cf5fead659c60d4228
lcj1204 <dlckdwo1204@naver.com>
2023-05-01 21:25:35 +0900

    [Feat]: 그룹 상세 정보 수정을 위한 requestDto 구현

---
+++ src/main/java/scs/planus/domain/group/dto/GroupDetailUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package scs.planus.domain.group.dto;
+
+import lombok.Getter;
+import scs.planus.domain.tag.dto.TagCreateRequestDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter
+public class GroupDetailUpdateRequestDto {
+
+    @Size(max = 5, message = "[request] 테그는 5개 이하로 입력해 주세요.")
+    private List<@Valid TagCreateRequestDto> tagList;
+
+    @Max(value = 50, message = "[request] 그룹 제한 인원은 50명 이하로 입력해 주세요.")
+    @Min(value = 2, message = "[request] 그룹 제한 인원은 2명 이상으로 입력해 주세요.")
+    private Long limitCount;
+}
