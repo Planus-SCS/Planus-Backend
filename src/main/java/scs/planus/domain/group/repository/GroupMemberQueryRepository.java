4b712ed8cdd1dbe282909658f21cbd4c5b426de3
leesangjin <tkdwls0628@gmail.com>
2023-05-03 02:21:22 +0900

    [Feat]: Query-dsl 조건식 메서드화 및 그룹 상태 조건식 추가

--- src/main/java/scs/planus/domain/group/repository/GroupMemberQueryRepository.java
+++ src/main/java/scs/planus/domain/group/repository/GroupMemberQueryRepository.java
@@ -1,9 +1,11 @@
 package scs.planus.domain.group.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import scs.planus.domain.Status;
 
 import static scs.planus.domain.group.entity.QGroup.group;
 import static scs.planus.domain.group.entity.QGroupMember.groupMember;
@@ -21,8 +23,20 @@ public class GroupMemberQueryRepository {
                 .from(groupMember)
                 .join(groupMember.member, member)
                 .join(groupMember.group, group)
-                .where(member.id.eq(memberId), group.id.eq(groupId))
+                .where(isActiveGroup(), memberIdEq(memberId), groupIdEq(groupId))
                 .fetchFirst();
         return fetchOne != null;
+    }
+
+    private BooleanExpression memberIdEq(Long memberId) {
+        return member.id.eq(memberId);
+    }
+
+    private BooleanExpression groupIdEq(Long groupId) {
+        return group.id.eq(groupId);
+    }
+
+    private BooleanExpression isActiveGroup() {
+        return group.status.eq(Status.ACTIVE);
     }
 }
