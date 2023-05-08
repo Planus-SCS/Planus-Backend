31c4fdf23fe27185beb9525702a540d5ef35cc08
lcj1204 <dlckdwo1204@naver.com>
2023-05-08 18:30:07 +0900

    [Feat]: List<Group> 에 포함되는 모든 GroupJoin 조회 쿼리 구현

--- src/main/java/scs/planus/domain/group/repository/GroupJoinRepository.java
+++ src/main/java/scs/planus/domain/group/repository/GroupJoinRepository.java
@@ -1,7 +1,19 @@
 package scs.planus.domain.group.repository;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import scs.planus.domain.group.entity.Group;
 import scs.planus.domain.group.entity.GroupJoin;
 
+import java.util.List;
+
 public interface GroupJoinRepository extends JpaRepository<GroupJoin, Long> {
+
+    @Query("select gj " +
+            "from GroupJoin gj " +
+            "join fetch gj.group " +
+            "join fetch gj.member " +
+            "where gj.group in :groups ")
+    List<GroupJoin> findAllByGroupIn(@Param("groups") List<Group> groups);
 }
