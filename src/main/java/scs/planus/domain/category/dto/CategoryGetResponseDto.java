49a00f795df555785dc29ce7234d9fabe58a1f0d
lcj1204 <dlckdwo1204@naver.com>
2023-05-09 14:40:32 +0900

    [Refactor]: 카테고리 응답 Dto 에 Status 필드 추가

--- src/main/java/scs/planus/domain/category/dto/CategoryGetResponseDto.java
+++ src/main/java/scs/planus/domain/category/dto/CategoryGetResponseDto.java
@@ -2,6 +2,7 @@ package scs.planus.domain.category.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import scs.planus.domain.Status;
 import scs.planus.domain.category.entity.Color;
 import scs.planus.domain.category.entity.TodoCategory;
 
@@ -11,12 +12,14 @@ public class CategoryGetResponseDto {
     private Long id;
     private String name;
     private Color color;
+    private Status status;
 
     public static CategoryGetResponseDto of(TodoCategory todoCategory) {
         return CategoryGetResponseDto.builder()
                 .id(todoCategory.getId())
                 .name(todoCategory.getName())
                 .color(todoCategory.getColor())
+                .status(todoCategory.getStatus())
                 .build();
     }
 }
