be86dc06ef3fe0036ea7fbf49a6764af54487fd2
leesangjin <tkdwls0628@gmail.com>
2023-05-12 18:12:34 +0900

    [Feat]: 속한 개별 그룹 조회용 응답 DTO 구현

---
+++ src/main/java/scs/planus/domain/group/dto/mygroup/MyGroupDetailResponseDto.java
@@ -0,0 +1,39 @@
+package scs.planus.domain.group.dto.mygroup;
+
+import lombok.Builder;
+import lombok.Getter;
+import scs.planus.domain.group.dto.GroupTagResponseDto;
+import scs.planus.domain.group.entity.Group;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class MyGroupDetailResponseDto {
+
+    private Long groupId;
+    private String groupImageUrl;
+    private String groupName;
+    private Boolean isOnline;
+    private int onlineCount;
+    private int memberCount;
+    private int limitCount;
+    private String leaderName;
+    private String notice;
+    private List<GroupTagResponseDto> groupTags;
+
+    public static MyGroupDetailResponseDto of(Group group, List<GroupTagResponseDto> eachGroupTagDtos, Boolean onlineStatus, int onlineCount) {
+        return MyGroupDetailResponseDto.builder()
+                .groupId(group.getId())
+                .groupImageUrl(group.getGroupImageUrl())
+                .groupName(group.getName())
+                .isOnline(onlineStatus)
+                .onlineCount(onlineCount)
+                .memberCount(group.getGroupMembers().size()) // 추가쿼리 발생
+                .limitCount(group.getLimitCount())
+                .leaderName(group.getLeaderName()) // 추가쿼리 발생
+                .notice(group.getNotice())
+                .groupTags(eachGroupTagDtos)
+                .build();
+    }
+}
