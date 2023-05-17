810a0a27343e8c9a1718d0dec20ba67686d5b129
lcj1204 <dlckdwo1204@naver.com>
2023-05-17 17:36:41 +0900

    [Feat]: 그룹 투두 권한 관련 예외 추가

--- src/main/java/scs/planus/global/exception/CustomExceptionStatus.java
+++ src/main/java/scs/planus/global/exception/CustomExceptionStatus.java
@@ -42,6 +42,7 @@ public enum CustomExceptionStatus implements ResponseStatus {
     EXCEED_GROUP_LIMIT_COUNT(BAD_REQUEST, 2603, "그룹 제한 인원을 초과하였습니다."),
     ALREADY_JOINED_GROUP(BAD_REQUEST, 2604, "이미 가입된 그룹입니다."),
     NOT_EXIST_GROUP_JOIN(BAD_REQUEST, 2605, "존재 하지 않는 그룹 가입 신청서 입니다."),
+    DO_NOT_HAVE_TODO_AUTHORITY(BAD_REQUEST, 2606, "그룹 투두 권한이 없습니다."),
 
     // groupMember excepion
     NOT_JOINED_GROUP(BAD_REQUEST, 2700, "가입하지 않은 그룹 입니다."),
