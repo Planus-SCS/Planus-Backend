d573faf728f8185749310a7b4d5b9ecda68335d0
leesangjin <tkdwls0628@gmail.com>
2023-05-18 21:02:21 +0900

    [Refactor]: GroupMember를 통한 예외처리로 변경

--- src/main/java/scs/planus/domain/category/service/GroupTodoCategoryService.java
+++ src/main/java/scs/planus/domain/category/service/GroupTodoCategoryService.java
@@ -11,10 +11,9 @@ import scs.planus.domain.category.entity.Color;
 import scs.planus.domain.category.entity.GroupTodoCategory;
 import scs.planus.domain.category.repository.TodoCategoryRepository;
 import scs.planus.domain.group.entity.Group;
-import scs.planus.domain.group.repository.GroupMemberQueryRepository;
+import scs.planus.domain.group.entity.GroupMember;
+import scs.planus.domain.group.repository.GroupMemberRepository;
 import scs.planus.domain.group.repository.GroupRepository;
-import scs.planus.domain.member.entity.Member;
-import scs.planus.domain.member.repository.MemberRepository;
 import scs.planus.global.exception.PlanusException;
 
 import java.util.List;
@@ -27,100 +26,100 @@ import static scs.planus.global.exception.CustomExceptionStatus.*;
 @RequiredArgsConstructor
 @Slf4j
 public class GroupTodoCategoryService {
+
     private final TodoCategoryRepository todoCategoryRepository;
-    private final MemberRepository memberRepository;
-    private final GroupMemberQueryRepository groupMemberQueryRepository;
     private final GroupRepository groupRepository;
+    private final GroupMemberRepository groupMemberRepository;
 
-    public List<TodoCategoryGetResponseDto> findAll(Long memberId, Long groupId ) {
-        Member member = memberRepository.findById( memberId )
-                .orElseThrow(() -> new PlanusException( NONE_USER ));
+    public List<TodoCategoryGetResponseDto> findAll(Long memberId, Long groupId) {
+        GroupMember groupMember = groupMemberRepository.findByMemberIdAndGroupId(memberId, groupId)
+                .orElseThrow(() -> {
+                    groupRepository.findById(groupId)
+                            .orElseThrow(() -> new PlanusException(NOT_EXIST_GROUP));
+                    return new PlanusException(NOT_JOINED_GROUP);
+                });
 
-        Group group = groupRepository.findByIdAndStatus( groupId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_GROUP ));
-
-        Boolean hasTodoAuthority = groupMemberQueryRepository
-                .existByMemberIdAndGroupIdAndTodoAuthority( member.getId(), group.getId() );
+        boolean hasTodoAuthority = groupMember.isTodoAuthority();
 
         if (!hasTodoAuthority) {
-            throw new PlanusException( DO_NOT_HAVE_TODO_AUTHORITY );
+            throw new PlanusException(DO_NOT_HAVE_TODO_AUTHORITY);
         }
 
-        List<GroupTodoCategory> groupTodoCategories = todoCategoryRepository.findGroupTodoCategoryAllByGroup( group );
+        List<GroupTodoCategory> groupTodoCategories = todoCategoryRepository.findGroupTodoCategoryAllByGroup(groupId);
 
         return groupTodoCategories.stream()
-                .map( TodoCategoryGetResponseDto::of )
-                .collect( Collectors.toList() );
+                .map(TodoCategoryGetResponseDto::of)
+                .collect(Collectors.toList());
     }
 
     @Transactional
-    public TodoCategoryResponseDto createCategory(Long memberId, Long groupId, TodoCategoryRequestDto requestDto ) {
-        Member member = memberRepository.findById( memberId )
-                .orElseThrow(() -> new PlanusException( NONE_USER ));
+    public TodoCategoryResponseDto createCategory(Long memberId, Long groupId, TodoCategoryRequestDto requestDto) {
+        GroupMember groupMember = groupMemberRepository.findByMemberIdAndGroupId(memberId, groupId)
+                .orElseThrow(() -> {
+                    groupRepository.findById(groupId)
+                            .orElseThrow(() -> new PlanusException(NOT_EXIST_GROUP));
+                    return new PlanusException(NOT_JOINED_GROUP);
+                });
 
-        Group group = groupRepository.findByIdAndStatus( groupId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_GROUP ));
-
-        Boolean hasTodoAuthority = groupMemberQueryRepository
-                .existByMemberIdAndGroupIdAndTodoAuthority( member.getId(), group.getId() );
+        Group group = groupMember.getGroup();
+        boolean hasTodoAuthority = groupMember.isTodoAuthority();
 
         if (!hasTodoAuthority) {
-            throw new PlanusException( DO_NOT_HAVE_TODO_AUTHORITY );
+            throw new PlanusException(DO_NOT_HAVE_TODO_AUTHORITY);
         }
 
         Color color = Color.of(requestDto.getColor());
 
-        GroupTodoCategory groupTodoCategory = requestDto.toGroupTodoCategoryEntity( group, color );
-        GroupTodoCategory saveGroupTodoCategory = todoCategoryRepository.save( groupTodoCategory );
+        GroupTodoCategory groupTodoCategory = requestDto.toGroupTodoCategoryEntity(group, color);
+        GroupTodoCategory saveGroupTodoCategory = todoCategoryRepository.save(groupTodoCategory);
 
-        return TodoCategoryResponseDto.of( saveGroupTodoCategory );
+        return TodoCategoryResponseDto.of(saveGroupTodoCategory);
     }
 
     @Transactional
-    public TodoCategoryResponseDto changeCategory(Long memberId, Long groupId, Long categoryId, TodoCategoryRequestDto requestDto ) {
-        Member member = memberRepository.findById( memberId )
-                .orElseThrow(() -> new PlanusException( NONE_USER ));
+    public TodoCategoryResponseDto changeCategory(Long memberId, Long groupId, Long categoryId, TodoCategoryRequestDto requestDto) {
+        GroupMember groupMember = groupMemberRepository.findByMemberIdAndGroupId(memberId, groupId)
+                .orElseThrow(() -> {
+                    groupRepository.findById(groupId)
+                            .orElseThrow(() -> new PlanusException(NOT_EXIST_GROUP));
+                    return new PlanusException(NOT_JOINED_GROUP);
+                });
 
-        Group group = groupRepository.findByIdAndStatus( groupId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_GROUP ));
-
-        Boolean hasTodoAuthority = groupMemberQueryRepository
-                .existByMemberIdAndGroupIdAndTodoAuthority( member.getId(), group.getId() );
+        boolean hasTodoAuthority = groupMember.isTodoAuthority();
 
         if (!hasTodoAuthority) {
-            throw new PlanusException( DO_NOT_HAVE_TODO_AUTHORITY );
+            throw new PlanusException(DO_NOT_HAVE_TODO_AUTHORITY);
         }
 
-        GroupTodoCategory groupTodoCategory = todoCategoryRepository.findGroupTodoCategoryByIdAndStatus( categoryId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_CATEGORY ));
+        GroupTodoCategory groupTodoCategory = todoCategoryRepository.findGroupTodoCategoryByIdAndStatus(categoryId)
+                .orElseThrow(() -> new PlanusException(NOT_EXIST_CATEGORY));
 
-        Color color = Color.of( requestDto.getColor() );
-        groupTodoCategory.change( requestDto.getName(), color );
+        Color color = Color.of(requestDto.getColor());
+        groupTodoCategory.change(requestDto.getName(), color);
 
-        return TodoCategoryResponseDto.of( groupTodoCategory );
+        return TodoCategoryResponseDto.of(groupTodoCategory);
     }
 
     @Transactional
-    public TodoCategoryResponseDto deleteCategory(Long memberId, Long groupId, Long categoryId ) {
-        Member member = memberRepository.findById( memberId )
-                .orElseThrow(() -> new PlanusException( NONE_USER ));
+    public TodoCategoryResponseDto deleteCategory(Long memberId, Long groupId, Long categoryId) {
+        GroupMember groupMember = groupMemberRepository.findByMemberIdAndGroupId(memberId, groupId)
+                .orElseThrow(() -> {
+                    groupRepository.findById(groupId)
+                            .orElseThrow(() -> new PlanusException(NOT_EXIST_GROUP));
+                    return new PlanusException(NOT_JOINED_GROUP);
+                });
 
-        Group group = groupRepository.findByIdAndStatus( groupId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_GROUP ));
-
-        Boolean hasTodoAuthority = groupMemberQueryRepository
-                .existByMemberIdAndGroupIdAndTodoAuthority( member.getId(), group.getId() );
+        boolean hasTodoAuthority = groupMember.isTodoAuthority();
 
         if (!hasTodoAuthority) {
-            throw new PlanusException( DO_NOT_HAVE_TODO_AUTHORITY );
+            throw new PlanusException(DO_NOT_HAVE_TODO_AUTHORITY);
         }
 
-        GroupTodoCategory groupTodoCategory = todoCategoryRepository.findGroupTodoCategoryByIdAndStatus( categoryId )
-                .orElseThrow(() -> new PlanusException( NOT_EXIST_CATEGORY ));
+        GroupTodoCategory groupTodoCategory = todoCategoryRepository.findGroupTodoCategoryByIdAndStatus(categoryId)
+                .orElseThrow(() -> new PlanusException(NOT_EXIST_CATEGORY));
 
         groupTodoCategory.changeStatusToInactive();
 
-        return TodoCategoryResponseDto.of( groupTodoCategory );
+        return TodoCategoryResponseDto.of(groupTodoCategory);
     }
-
 }
