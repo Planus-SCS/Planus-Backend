f1aba9e9139165b69667615ad8cce978edfee227
leesangjin <tkdwls0628@gmail.com>
2023-05-19 02:37:02 +0900

    [Feat]: 그룹 투두 월별 조회 API 구현

---
+++ src/main/java/scs/planus/domain/todo/controller/GroupTodoCalendarController.java
@@ -0,0 +1,41 @@
+package scs.planus.domain.todo.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import scs.planus.domain.todo.dto.TodoPeriodResponseDto;
+import scs.planus.domain.todo.service.GroupTodoCalendarService;
+import scs.planus.global.auth.entity.PrincipalDetails;
+import scs.planus.global.common.response.BaseResponse;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "GroupTodo Calendar", description = "GroupTodo Calendar API Document")
+public class GroupTodoCalendarController {
+
+    private final GroupTodoCalendarService GroupTodoCalendarService;
+
+    @GetMapping("my-groups/{groupId}/todos/calendar")
+    @Operation(summary = "월별 GroupTodo Calendar 조회 API")
+    public BaseResponse<List<TodoPeriodResponseDto>> getPeriodTodos(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                                    @PathVariable Long groupId,
+                                                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from,
+                                                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate to) {
+        Long memberId = principalDetails.getId();
+        List<TodoPeriodResponseDto> responseDtos = GroupTodoCalendarService.getPeriodGroupTodos(memberId, groupId, from, to);
+        return new BaseResponse<>(responseDtos);
+    }
+}
