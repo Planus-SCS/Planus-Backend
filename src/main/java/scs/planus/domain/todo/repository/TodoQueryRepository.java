e371a0fd1c156828144af762b97e43cef05bd555
leesangjin <tkdwls0628@gmail.com>
2023-05-18 21:22:25 +0900

    [Feat]: 그룹 단일 투두 조회 쿼리 구현

--- src/main/java/scs/planus/domain/todo/repository/TodoQueryRepository.java
+++ src/main/java/scs/planus/domain/todo/repository/TodoQueryRepository.java
@@ -5,6 +5,8 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import scs.planus.domain.Status;
+import scs.planus.domain.todo.entity.GroupTodo;
 import scs.planus.domain.todo.entity.MemberTodo;
 
 import java.time.LocalDate;
@@ -14,6 +16,7 @@ import java.util.Optional;
 import static scs.planus.domain.category.entity.QTodoCategory.todoCategory;
 import static scs.planus.domain.group.entity.QGroup.group;
 import static scs.planus.domain.member.entity.QMember.member;
+import static scs.planus.domain.todo.entity.QGroupTodo.groupTodo;
 import static scs.planus.domain.todo.entity.QMemberTodo.memberTodo;
 
 @Repository
@@ -87,12 +90,25 @@ public class TodoQueryRepository {
                 .fetch();
     }
 
+    public Optional<GroupTodo> findOneGroupTodoById(Long groupId, Long todoId) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(groupTodo)
+                .join(groupTodo.group, group).fetchJoin()
+                .join(groupTodo.todoCategory, todoCategory).fetchJoin()
+                .where(isActiveGroup(), groupTodo.id.eq(todoId), groupIdEq(groupId))
+                .fetchOne());
+    }
+
     private BooleanExpression memberIdEq(Long memberId) {
         return member.id.eq(memberId);
     }
 
     private BooleanExpression groupIdEq(Long groupId) {
         return group.id.eq(groupId);
+    }
+
+    private BooleanExpression isActiveGroup() {
+        return group.status.eq(Status.ACTIVE);
     }
 
     private BooleanExpression dateBetween(LocalDate date) {
