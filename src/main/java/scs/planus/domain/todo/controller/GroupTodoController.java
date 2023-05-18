7b56d4d035511f0fbe774f493606ae13a0180820
leesangjin <tkdwls0628@gmail.com>
2023-05-18 21:23:16 +0900

    [Feat]: 그룹 단일 투두 조회 API 구현

--- src/main/java/scs/planus/domain/todo/controller/GroupTodoController.java
+++ src/main/java/scs/planus/domain/todo/controller/GroupTodoController.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import scs.planus.domain.todo.dto.TodoDetailsResponseDto;
 import scs.planus.domain.todo.dto.TodoRequestDto;
 import scs.planus.domain.todo.dto.TodoResponseDto;
 import scs.planus.domain.todo.service.GroupTodoService;
@@ -34,6 +36,16 @@ public class GroupTodoController {
                                                     @Valid @RequestBody TodoRequestDto todoRequestDto) {
         Long memberId = principalDetails.getId();
         TodoResponseDto responseDto = groupTodoService.createGroupTodo(memberId, groupId, todoRequestDto);
+        return new BaseResponse<>(responseDto);
+    }
+
+    @GetMapping("/my-groups/{groupId}/todos/{todoId}")
+    @Operation(summary = "Group Todo 조회 API")
+    public BaseResponse<TodoDetailsResponseDto>  getTodoDetail(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                               @PathVariable Long groupId,
+                                                               @PathVariable Long todoId) {
+        Long memberId = principalDetails.getId();
+        TodoDetailsResponseDto responseDto = groupTodoService.getOneTodo(memberId, groupId, todoId);
         return new BaseResponse<>(responseDto);
     }
 }
