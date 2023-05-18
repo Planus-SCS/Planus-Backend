6b61e50d525a2bc7ff43aa5e7316bbd1805a432c
leesangjin <tkdwls0628@gmail.com>
2023-05-18 18:40:40 +0900

    [Feat]: 그룹투두 생성 기능 구현

---
+++ src/main/java/scs/planus/domain/todo/service/GroupTodoService.java
@@ -0,0 +1,65 @@
+package scs.planus.domain.todo.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import scs.planus.domain.category.entity.GroupTodoCategory;
+import scs.planus.domain.category.repository.TodoCategoryRepository;
+import scs.planus.domain.group.entity.Group;
+import scs.planus.domain.group.entity.GroupMember;
+import scs.planus.domain.group.repository.GroupMemberQueryRepository;
+import scs.planus.domain.group.repository.GroupMemberRepository;
+import scs.planus.domain.group.repository.GroupRepository;
+import scs.planus.domain.todo.dto.TodoRequestDto;
+import scs.planus.domain.todo.dto.TodoResponseDto;
+import scs.planus.domain.todo.entity.GroupTodo;
+import scs.planus.domain.todo.entity.GroupTodoCompletion;
+import scs.planus.domain.todo.repository.GroupTodoCompletionRepository;
+import scs.planus.domain.todo.repository.TodoRepository;
+import scs.planus.global.exception.PlanusException;
+
+import java.util.List;
+
+import static scs.planus.global.exception.CustomExceptionStatus.*;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class GroupTodoService {
+
+    private final GroupRepository groupRepository;
+    private final GroupMemberQueryRepository groupMemberQueryRepository;
+    private final GroupMemberRepository groupMemberRepository;
+    private final TodoCategoryRepository todoCategoryRepository;
+    private final TodoRepository todoRepository;
+    private final GroupTodoCompletionRepository groupTodoCompletionRepository;
+
+    @Transactional
+    public TodoResponseDto createGroupTodo(Long memberId, Long groupId, TodoRequestDto requestDto) {
+        Group group = groupRepository.findByIdAndStatus(groupId)
+                .orElseThrow(() -> new PlanusException(NOT_EXIST_GROUP));
+
+        Boolean hasTodoAuthority = groupMemberQueryRepository.existByMemberIdAndGroupIdAndTodoAuthority(memberId, group.getId());
+
+        if (!hasTodoAuthority) {
+            throw new PlanusException(DO_NOT_HAVE_TODO_AUTHORITY);
+        }
+
+        GroupTodoCategory groupTodoCategory = todoCategoryRepository.findGroupTodoCategoryByIdAndStatus(requestDto.getCategoryId())
+                .orElseThrow(() -> new PlanusException(NOT_EXIST_CATEGORY));
+
+        GroupTodo groupTodo = requestDto.toGroupTodoEntity(group, groupTodoCategory);
+
+        List<GroupMember> groupMembers = groupMemberRepository.findAllWithMemberByGroupAndStatus(group);
+        groupMembers.stream()
+                .map(GroupMember::getMember)
+                .forEach(member -> {
+                    GroupTodoCompletion.createGroupTodoCompletion(member, groupTodo);
+                });
+
+        todoRepository.save(groupTodo);
+        return TodoResponseDto.of(groupTodo);
+    }
+}
