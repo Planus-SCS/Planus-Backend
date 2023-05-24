d998801d1012b73de7d24d4010ff05177bbf1bb0
lcj1204 <dlckdwo1204@naver.com>
2023-05-24 19:39:21 +0900

    [Feat]: 그룹 검색화면을 위한 GroupGetResponseDto 구현

---
+++ src/main/java/scs/planus/domain/group/dto/GroupsGetResponseDto.java
@@ -0,0 +1,34 @@
+package scs.planus.domain.group.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import scs.planus.domain.group.entity.Group;
+import scs.planus.domain.member.entity.Member;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class GroupsGetResponseDto {
+    private Long groupId;
+    private String name;
+    private String groupImageUrl;
+    private int memberCount;
+    private int limitCount;
+    private Long leaderId;
+    private String leaderName;
+    private List<GroupTagResponseDto> groupTags;
+
+    public static GroupsGetResponseDto of( Group group, int memberCount, Member groupLeader, List<GroupTagResponseDto> groupTagNameList ) {
+        return GroupsGetResponseDto.builder()
+                .groupId( group.getId() )
+                .name( group.getName() )
+                .groupImageUrl( group.getGroupImageUrl() )
+                .memberCount( memberCount )
+                .limitCount( group.getLimitCount() )
+                .leaderId( groupLeader.getId() )
+                .leaderName( groupLeader.getNickname() )
+                .groupTags( groupTagNameList )
+                .build();
+    }
+}
