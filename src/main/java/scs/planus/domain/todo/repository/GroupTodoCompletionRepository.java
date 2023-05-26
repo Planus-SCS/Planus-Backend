aab5c6b3ecc88d065c124d1209796d16379ae3d9
leesangjin <tkdwls0628@gmail.com>
2023-05-26 15:29:37 +0900

    [Feat]: GroupTodoCompletion 조회 쿼리 구현

--- src/main/java/scs/planus/domain/todo/repository/GroupTodoCompletionRepository.java
+++ src/main/java/scs/planus/domain/todo/repository/GroupTodoCompletionRepository.java
@@ -1,7 +1,18 @@
 package scs.planus.domain.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import scs.planus.domain.todo.entity.GroupTodoCompletion;
 
+import java.util.Optional;
+
 public interface GroupTodoCompletionRepository extends JpaRepository<GroupTodoCompletion, Long> {
+
+    @Query("select gtc from GroupTodoCompletion gtc " +
+            "join gtc.member m " +
+            "join gtc.groupTodo gt " +
+            "where m.id= :memberId and gt.id = :todoId")
+    Optional<GroupTodoCompletion> findByMemberIdAndTodoId(@Param("memberId") Long memberId,
+                                                          @Param("todoId") Long todoId);
 }
