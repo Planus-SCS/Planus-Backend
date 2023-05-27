8cf6c63722b4e772b892175a4d974be27f06fde4
leesangjin <tkdwls0628@gmail.com>
2023-05-27 21:44:05 +0900

    [Refactor]: @JsonInclude를 통해 null인 값을 응답값에서 보이지 않도록 수정

--- src/main/java/scs/planus/domain/todo/dto/calendar/TodoDailyDto.java
+++ src/main/java/scs/planus/domain/todo/dto/calendar/TodoDailyDto.java
@@ -1,9 +1,14 @@
 package scs.planus.domain.todo.dto.calendar;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import scs.planus.domain.category.entity.Color;
+import scs.planus.domain.todo.entity.MemberTodo;
 import scs.planus.domain.todo.entity.Todo;
+
+import java.time.LocalTime;
 
 @Getter
 @Builder
@@ -13,9 +18,15 @@ public class TodoDailyDto {
     private Color categoryColor;
     private String title;
 
+    @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private LocalTime startTime;
+
     private Boolean hasGroup;
     private Boolean isPeriodTodo;
     private Boolean hasDescription;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Boolean isCompleted;
 
     public static TodoDailyDto of(Todo todo) {
@@ -23,6 +34,7 @@ public class TodoDailyDto {
                 .todoId(todo.getId())
                 .categoryColor(todo.getTodoCategory().getColor())
                 .title(todo.getTitle())
+                .startTime(todo.getStartTime())
                 .hasGroup(todo.getGroup() != null)
                 .isPeriodTodo(todo.getEndDate().isAfter(todo.getStartDate()))
                 .hasDescription(todo.getDescription() != null)
