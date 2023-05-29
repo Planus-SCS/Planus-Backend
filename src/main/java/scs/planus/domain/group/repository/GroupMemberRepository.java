33e71cb5452b8080ecf409572d75d8410f51a9d4
leesangjin <tkdwls0628@gmail.com>
2023-05-30 00:30:13 +0900

    [Fix]: 나의 그룹 가져올 때, GroupMember status가 ACTIVE인 것들만 나오도록 수정

--- src/main/java/scs/planus/domain/group/repository/GroupMemberRepository.java
+++ src/main/java/scs/planus/domain/group/repository/GroupMemberRepository.java
@@ -37,7 +37,7 @@ public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> 
     @Query("select gm from GroupMember gm " +
             "join fetch gm.group g " +
             "join fetch gm.member m " +
-            "where g.status = 'ACTIVE' and m.id = :memberId")
+            "where g.status = 'ACTIVE' and gm.status = 'ACTIVE' and m.id = :memberId")
     List<GroupMember> findAllByActiveGroupAndMemberId(@Param("memberId") Long memberId);
 
     @Query("select gm from GroupMember gm " +
