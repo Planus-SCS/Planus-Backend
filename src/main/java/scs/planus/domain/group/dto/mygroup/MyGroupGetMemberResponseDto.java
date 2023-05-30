3c8eac1509e7fde64c0a00546f352572fb030c29
leesangjin <tkdwls0628@gmail.com>
2023-05-30 18:57:25 +0900

    [Remove]: 사용하지 않는 생성메서드 제거

--- src/main/java/scs/planus/domain/group/dto/mygroup/MyGroupGetMemberResponseDto.java
+++ src/main/java/scs/planus/domain/group/dto/mygroup/MyGroupGetMemberResponseDto.java
@@ -3,7 +3,6 @@ package scs.planus.domain.group.dto.mygroup;
 import lombok.Builder;
 import lombok.Getter;
 import scs.planus.domain.group.entity.GroupMember;
-import scs.planus.domain.member.entity.Member;
 
 @Getter
 @Builder
@@ -15,17 +14,6 @@ public class MyGroupGetMemberResponseDto {
     private Boolean isOnline;
     private String description;
     private String profileImageUrl;
-
-    public static MyGroupGetMemberResponseDto of(Member member, Boolean isLeader, Boolean isOnline) {
-        return MyGroupGetMemberResponseDto.builder()
-                .memberId(member.getId())
-                .nickname(member.getNickname())
-                .isLeader(isLeader)
-                .isOnline(isOnline)
-                .description(member.getDescription())
-                .profileImageUrl(member.getProfileImageUrl())
-                .build();
-    }
 
     public static MyGroupGetMemberResponseDto of(GroupMember groupMember) {
         return MyGroupGetMemberResponseDto.builder()
