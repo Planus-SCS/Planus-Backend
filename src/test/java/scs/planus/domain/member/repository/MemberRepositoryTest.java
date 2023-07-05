45aa4da063f1b064e30d061d40f09d2c7c1b1115
leesangjin <tkdwls0628@gmail.com>
2023-07-05 17:55:18 +0900

    [Refactor]: 사용하지 않는 어노테이션 제거

--- src/test/java/scs/planus/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/scs/planus/domain/member/repository/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package scs.planus.domain.member.repository;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +12,6 @@ import scs.planus.domain.member.entity.Role;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Slf4j
 class MemberRepositoryTest {
 
     @Autowired
