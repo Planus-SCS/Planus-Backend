73eaa1c1998861058fb8925e8d56dbe2d14914f8
leesangjin <tkdwls0628@gmail.com>
2023-07-08 00:50:51 +0900

    [Test]: GroupTodoCompletion 생성 메서드 테스트코드 추가

--- src/test/java/scs/planus/domain/todo/entity/GroupTodoCompletionTest.java
+++ src/test/java/scs/planus/domain/todo/entity/GroupTodoCompletionTest.java
@@ -1,8 +1,10 @@
 package scs.planus.domain.todo.entity;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import scs.planus.domain.member.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GroupTodoCompletionTest {
 
@@ -18,7 +20,7 @@ class GroupTodoCompletionTest {
         groupTodoCompletion.changeCompletion();
 
         //then
-        Assertions.assertThat(groupTodoCompletion.isCompletion()).isFalse();
+        assertThat(groupTodoCompletion.isCompletion()).isFalse();
     }
 
     @DisplayName("그룹 투두 완료 상태를 false에서 true로 변경할 수 있어야 한다.")
@@ -33,7 +35,27 @@ class GroupTodoCompletionTest {
         groupTodoCompletion.changeCompletion();
 
         //then
-        Assertions.assertThat(groupTodoCompletion.isCompletion()).isTrue();
+        assertThat(groupTodoCompletion.isCompletion()).isTrue();
+    }
+
+    @DisplayName("그룹 투두 완료가 제대로 생성되어야 한다.")
+    @Test
+    void createGroupTodoCompletion(){
+        //given
+        Member member = Member.builder()
+                .name("member")
+                .build();
+
+        GroupTodo groupTodo = GroupTodo.builder()
+                .title("groupTodo")
+                .build();
+
+        //when
+        GroupTodoCompletion groupTodoCompletion = GroupTodoCompletion.createGroupTodoCompletion(member, groupTodo);
+
+        //then
+        assertThat(groupTodoCompletion.getMember()).isEqualTo(member);
+        assertThat(groupTodoCompletion.getGroupTodo()).isEqualTo(groupTodo);
     }
 
 }
