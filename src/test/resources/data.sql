4a3026d167a8bcdbde196416b8651e22a1ba484c
leesangjin <tkdwls0628@gmail.com>
2023-07-14 21:31:22 +0900

    [Feat]: data.sql에 기본 Group 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,5 @@
 insert into member (description, email, nickname, profile_image_url, role, social_type, status)
 values('hello planus!', 'planus@planus','planus', 'image_url', 'USER', 'KAKAO', 'ACTIVE');
+
+insert into _group (group_image_url, introduction, limit_count, name, notice, scope, status)
+values('group_image_url', 'planus test group', 5, 'planus group', 'notice', 'PUBLIC', 'ACTIVE');
