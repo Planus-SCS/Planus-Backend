591eb19320f290623f9571e24682686cf526a8c6
leesangjin <tkdwls0628@gmail.com>
2023-07-14 21:31:46 +0900

    [Refactor]: 추상 클래스 대신 애너테이션으로 변경

--- src/test/java/scs/planus/support/RepositoryTest.java
+++ src/test/java/scs/planus/support/RepositoryTest.java
@@ -1,50 +1,16 @@
 package scs.planus.support;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
-import scs.planus.domain.category.repository.TodoCategoryRepository;
-import scs.planus.domain.group.repository.GroupMemberRepository;
-import scs.planus.domain.group.repository.GroupRepository;
-import scs.planus.domain.member.repository.MemberRepository;
-import scs.planus.domain.todo.repository.GroupTodoCompletionRepository;
-import scs.planus.domain.todo.repository.TodoQueryRepository;
-import scs.planus.domain.todo.repository.TodoRepository;
-import scs.planus.global.config.QueryDslConfig;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 @DataJpaTest
 @Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
-@Import(QueryDslConfig.class)
-public abstract class RepositoryTest {
-
-    protected TodoQueryRepository todoQueryRepository;
-
-    @Autowired
-    protected JPAQueryFactory queryFactory;
-
-    @Autowired
-    protected MemberRepository memberRepository;
-
-    @Autowired
-    protected GroupRepository groupRepository;
-
-    @Autowired
-    protected GroupMemberRepository groupMemberRepository;
-
-    @Autowired
-    protected TodoRepository todoRepository;
-
-    @Autowired
-    protected TodoCategoryRepository todoCategoryRepository;
-
-    @Autowired
-    protected GroupTodoCompletionRepository groupTodoCompletionRepository;
-
-    @BeforeEach
-    void initRepository() {
-        todoQueryRepository = new TodoQueryRepository(queryFactory);
-    }
+public @interface RepositoryTest {
 }
--- src/test/java/scs/planus/support/ServiceTest.java
+++ src/test/java/scs/planus/support/ServiceTest.java
@@ -1,46 +1,16 @@
 package scs.planus.support;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
-import scs.planus.domain.category.repository.TodoCategoryRepository;
-import scs.planus.domain.group.repository.GroupMemberRepository;
-import scs.planus.domain.group.repository.GroupRepository;
-import scs.planus.domain.member.repository.MemberRepository;
-import scs.planus.domain.todo.repository.TodoQueryRepository;
-import scs.planus.domain.todo.repository.TodoRepository;
-import scs.planus.global.config.QueryDslConfig;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 @DataJpaTest
 @Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
-@Import(QueryDslConfig.class)
-public abstract class ServiceTest {
-
-    protected TodoQueryRepository todoQueryRepository;
-
-    @Autowired
-    protected JPAQueryFactory queryFactory;
-
-    @Autowired
-    protected MemberRepository memberRepository;
-
-    @Autowired
-    protected TodoCategoryRepository todoCategoryRepository;
-
-    @Autowired
-    protected TodoRepository todoRepository;
-
-    @Autowired
-    protected GroupMemberRepository groupMemberRepository;
-
-    @Autowired
-    protected GroupRepository groupRepository;
-
-    @BeforeEach
-    void initRepository() {
-        todoQueryRepository = new TodoQueryRepository(queryFactory);
-    }
+public @interface ServiceTest {
 }
