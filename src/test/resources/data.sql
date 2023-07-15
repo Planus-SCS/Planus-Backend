2b79c827d48656e5f1caf47e571af56c96d628a5
leesangjin <tkdwls0628@gmail.com>
2023-07-15 18:24:26 +0900

    [Feat]: data.sql에 기본 (Member/Group)TodoCategory 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,3 +3,9 @@ values('hello planus!', 'planus@planus','planus', 'image_url', 'USER', 'KAKAO', 
 
 insert into _group (group_image_url, introduction, limit_count, name, notice, scope, status)
 values('group_image_url', 'planus test group', 5, 'planus group', 'notice', 'PUBLIC', 'ACTIVE');
+
+insert into todo_category (dtype, color, name, status, member_id)
+values ('MC', 'RED', 'memberTodoCategory', 'ACTIVE', 1);
+
+insert into todo_category (dtype, color, name, status, member_id, group_id)
+values ('GC', 'BLUE', 'groupTodoCategory', 'ACTIVE', 1, 1);
