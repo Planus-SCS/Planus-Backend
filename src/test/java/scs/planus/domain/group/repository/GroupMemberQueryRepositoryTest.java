b628f7793d4039d982dac8d7fa3e354b9c91ad16
leesangjin <tkdwls0628@gmail.com>
2023-07-19 17:39:10 +0900

    [Feat]: GroupMemberQueryRespository 테스트코드 작성

---
+++ src/test/java/scs/planus/domain/group/repository/GroupMemberQueryRepositoryTest.java
@@ -0,0 +1,95 @@
+package scs.planus.domain.group.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import scs.planus.domain.Status;
+import scs.planus.domain.group.entity.Group;
+import scs.planus.domain.group.entity.GroupMember;
+import scs.planus.domain.member.entity.Member;
+import scs.planus.domain.member.repository.MemberRepository;
+import scs.planus.global.config.QueryDslConfig;
+import scs.planus.support.RepositoryTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RepositoryTest
+@Import(QueryDslConfig.class)
+class GroupMemberQueryRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    private GroupMemberQueryRepository groupMemberQueryRepository;
+
+    @BeforeEach
+    void init() {
+        groupMemberQueryRepository = new GroupMemberQueryRepository(queryFactory);
+    }
+
+    @DisplayName("GroupMember가 존재하면 true를 반환한다.")
+    @Test
+    void existByMemberIdAndGroupId_Return_True_If_GroupMember(){
+        //given
+        Member member = Member.builder().status(Status.ACTIVE).build();
+        Group group = Group.builder().status(Status.ACTIVE).build();
+
+        memberRepository.save(member);
+        groupRepository.save(group);
+
+        GroupMember.createGroupMember(member, group);
+
+        //when
+        Boolean isExisted
+                = groupMemberQueryRepository.existByMemberIdAndGroupId(member.getId(), group.getId());
+
+        //then
+        assertThat(isExisted).isTrue();
+    }
+
+    @DisplayName("GroupMember가 존재하지 않다면 false를 반환한다.")
+    @Test
+    void existByMemberIdAndGroupId_Return_False_If_Not_GroupMember(){
+        //given
+        Member member = Member.builder().status(Status.ACTIVE).build();
+        Group group = Group.builder().status(Status.ACTIVE).build();
+
+        memberRepository.save(member);
+        groupRepository.save(group);
+
+        //when
+        Boolean isExisted
+                = groupMemberQueryRepository.existByMemberIdAndGroupId(member.getId(), group.getId());
+
+        //then
+        assertThat(isExisted).isFalse();
+    }
+
+    @DisplayName("GroupMember의 status가 inactive라면 false를 반환한다.")
+    @Test
+    void existByMemberIdAndGroupId_Return_False_If_GroupMember_Status_Inactive(){
+        //given
+        Member member = Member.builder().status(Status.ACTIVE).build();
+        Group group = Group.builder().status(Status.ACTIVE).build();
+
+        memberRepository.save(member);
+        groupRepository.save(group);
+
+        GroupMember groupMember = GroupMember.createGroupMember(member, group);
+        groupMember.changeStatusToInactive();
+
+        //when
+        Boolean isExisted
+                = groupMemberQueryRepository.existByMemberIdAndGroupId(member.getId(), group.getId());
+
+        //then
+        assertThat(isExisted).isFalse();
+    }
+}
