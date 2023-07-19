c3ddef6fc21213204b7821dbfacad39f5a232a02
lcj1204 <dlckdwo1204@naver.com>
2023-07-19 17:46:15 +0900

    [Refactor]: data.sql 적용

--- src/test/java/scs/planus/domain/category/repository/TodoCategoryRepositoryTest.java
+++ src/test/java/scs/planus/domain/category/repository/TodoCategoryRepositoryTest.java
@@ -46,23 +46,23 @@ class TodoCategoryRepositoryTest {
         @Test
         void findMemberTodoCategoryByIdAndMember_Exist() {
             //given
-            MemberTodoCategory testMemberTodoCategory = MemberTodoCategory.builder()
+            MemberTodoCategory memberTodoCategory = MemberTodoCategory.builder()
                     .member(member)
                     .name("회원카테고리")
                     .color(Color.BLUE)
                     .build();
 
-            todoCategoryRepository.save(testMemberTodoCategory);
+            todoCategoryRepository.save(memberTodoCategory);
 
             //when
             MemberTodoCategory findMemberTodoCategory = todoCategoryRepository
-                    .findMemberTodoCategoryByMemberIdAndId(member.getId(), testMemberTodoCategory.getId())
+                    .findMemberTodoCategoryByMemberIdAndId(member.getId(), memberTodoCategory.getId())
                     .orElseThrow();
 
             //then
-            assertThat(findMemberTodoCategory.getMember().getId()).isEqualTo(testMemberTodoCategory.getMember().getId());
-            assertThat(findMemberTodoCategory.getName()).isEqualTo(testMemberTodoCategory.getName());
-            assertThat(findMemberTodoCategory.getColor()).isEqualTo(testMemberTodoCategory.getColor());
+            assertThat(findMemberTodoCategory.getMember().getId()).isEqualTo(memberTodoCategory.getMember().getId());
+            assertThat(findMemberTodoCategory.getName()).isEqualTo(memberTodoCategory.getName());
+            assertThat(findMemberTodoCategory.getColor()).isEqualTo(memberTodoCategory.getColor());
         }
 
         @DisplayName("TodoCategoryId, Member 로 조회되는 회원의 카테고리가 없는 경우 Null 을 반환 해야 한다.")
@@ -82,39 +82,11 @@ class TodoCategoryRepositoryTest {
         @Test
         void findMemberTodoCategoryAllByMember_Exist() {
             //given
-            MemberTodoCategory testMemberTodoCategory1 = MemberTodoCategory.builder()
-                    .member(member)
-                    .name("회원카테고리1")
-                    .color(Color.BLUE)
-                    .build();
-
-            MemberTodoCategory testMemberTodoCategory2 = MemberTodoCategory.builder()
-                    .member(member)
-                    .name("회원카테고리2")
-                    .color(Color.RED)
-                    .build();
-
-            todoCategoryRepository.saveAll(List.of(testMemberTodoCategory1, testMemberTodoCategory2));
-
             //when
             List<MemberTodoCategory> findMemberTodoCategories = todoCategoryRepository.findMemberTodoCategoryAllByMember(member);
 
             //then
-            assertThat(findMemberTodoCategories.size()).isEqualTo(2);
-            assertThat(findMemberTodoCategories.get(0).getName()).isEqualTo(testMemberTodoCategory1.getName());
-            assertThat(findMemberTodoCategories.get(1).getName()).isEqualTo(testMemberTodoCategory2.getName());
-
-        }
-
-        @DisplayName("Member 로 조회되는 회원의 카테고리가 존재하지 않는 경우 빈 리스트를 반환 해야 한다.")
-        @Test
-        void findMemberTodoCategoryAllByMember_Empty() {
-            //given
-            //when
-            List<MemberTodoCategory> findMemberTodoCategory = todoCategoryRepository.findMemberTodoCategoryAllByMember(member);
-
-            //then
-            assertThat(findMemberTodoCategory.size()).isEqualTo(0);
+            assertThat(findMemberTodoCategories).hasSize(1);
         }
     }
 
@@ -131,63 +103,34 @@ class TodoCategoryRepositoryTest {
         @Test
         void findGroupTodoCategoryAllByGroup_Exist() {
             //given
-            GroupTodoCategory testGroupTodoCategory1 = GroupTodoCategory.builder()
-                    .group(group)
-                    .name("그룹카테고리1")
-                    .color(Color.BLUE)
-                    .build();
-
-            GroupTodoCategory testGroupTodoCategory2 = GroupTodoCategory.builder()
-                    .group(group)
-                    .name("그룹카테고리2")
-                    .color(Color.RED)
-                    .build();
-
-            todoCategoryRepository.saveAll(List.of(testGroupTodoCategory1, testGroupTodoCategory2));
-
             //when
-            List<GroupTodoCategory> findGroupTodoCategories = todoCategoryRepository
-                    .findGroupTodoCategoryAllByGroup(group.getId());
+            List<GroupTodoCategory> findGroupTodoCategories = todoCategoryRepository.findGroupTodoCategoryAllByGroup(group.getId());
 
             //then
-            assertThat(findGroupTodoCategories.size()).isEqualTo(2);
-            assertThat(findGroupTodoCategories.get(0).getName()).isEqualTo(testGroupTodoCategory1.getName());
-            assertThat(findGroupTodoCategories.get(1).getName()).isEqualTo(testGroupTodoCategory2.getName());
-        }
-
-        @DisplayName("Group 으로 조회되는 그룹의 카테고리가 존재하지 않는 경우 빈 리스트를 반환 해야 한다.")
-        @Test
-        void findGroupTodoCategoryAllByGroup_Empty() {
-            //given
-            //when
-            List<GroupTodoCategory> findGroupTodoCategories = todoCategoryRepository
-                    .findGroupTodoCategoryAllByGroup(group.getId());
-
-            //then
-            assertThat(findGroupTodoCategories.size()).isEqualTo(0);
+            assertThat(findGroupTodoCategories).hasSize(1);
         }
 
         @DisplayName("TodoCategoryId 로 그룹의 카테고리를 조회할 수 있어야 한다.")
         @Test
         void findGroupTodoCategoryByIdAndStatus_Exist() {
             //given
-            GroupTodoCategory testGroupTodoCategory = GroupTodoCategory.builder()
+            GroupTodoCategory groupTodoCategory = GroupTodoCategory.builder()
                     .group(group)
                     .name("그룹카테고리")
                     .color(Color.BLUE)
                     .build();
 
-            todoCategoryRepository.save(testGroupTodoCategory);
+            todoCategoryRepository.save(groupTodoCategory);
 
             //when
             GroupTodoCategory findGroupTodoCategory = todoCategoryRepository
-                    .findGroupTodoCategoryByIdAndStatus(testGroupTodoCategory.getId())
+                    .findGroupTodoCategoryByIdAndStatus(groupTodoCategory.getId())
                     .orElseThrow();
 
             //then
-            assertThat(findGroupTodoCategory.getGroup()).isEqualTo(testGroupTodoCategory.getGroup());
-            assertThat(findGroupTodoCategory.getName()).isEqualTo(testGroupTodoCategory.getName());
-            assertThat(findGroupTodoCategory.getColor()).isEqualTo(testGroupTodoCategory.getColor());
+            assertThat(findGroupTodoCategory.getGroup()).isEqualTo(groupTodoCategory.getGroup());
+            assertThat(findGroupTodoCategory.getName()).isEqualTo(groupTodoCategory.getName());
+            assertThat(findGroupTodoCategory.getColor()).isEqualTo(groupTodoCategory.getColor());
         }
 
         @DisplayName("TodoCategoryId 로 조회되는 그룹의 카테고리가 없는 경우 Null 을 반환 해야 한다.")
@@ -213,19 +156,13 @@ class TodoCategoryRepositoryTest {
 
             groupRepository.save(group2);
 
-            GroupTodoCategory testGroup1TodoCategory = GroupTodoCategory.builder()
-                    .group(group)
-                    .name("그룹1카테고리")
-                    .color(Color.BLUE)
-                    .build();
-
             GroupTodoCategory testGroup2TodoCategory = GroupTodoCategory.builder()
                     .group(group2)
                     .name("그룹2카테고리")
                     .color(Color.RED)
                     .build();
 
-            todoCategoryRepository.saveAll(List.of(testGroup1TodoCategory, testGroup2TodoCategory));
+            todoCategoryRepository.save(testGroup2TodoCategory);
 
             List<Group> groups = List.of(group, group2);
 
@@ -234,30 +171,9 @@ class TodoCategoryRepositoryTest {
                     .findAllGroupTodoCategoriesInGroups(groups);
 
             //then
-            assertThat(findAllGroupTodoCategories.size()).isEqualTo(2);
+            assertThat(findAllGroupTodoCategories).hasSize(2);
             assertThat(findAllGroupTodoCategories.get(0).getGroup()).isEqualTo(group);
             assertThat(findAllGroupTodoCategories.get(1).getGroup()).isEqualTo(group2);
-        }
-
-        @DisplayName("List<Group> 으로 조회되는 그룹의 카테고리가 존재하지 않는 경우 빈 리스트를 반환 해야 한다.")
-        @Test
-        void findAllGroupTodoCategoriesInGroups_Empty() {
-            //given
-            Group group2 = Group.builder()
-                    .name("그룹2")
-                    .status(Status.ACTIVE)
-                    .build();
-
-            groupRepository.save(group2);
-
-            List<Group> groups = List.of(group, group2);
-
-            //when
-            List<GroupTodoCategory> findAllGroupTodoCategories = todoCategoryRepository
-                    .findAllGroupTodoCategoriesInGroups(groups);
-
-            //then
-            assertThat(findAllGroupTodoCategories.size()).isEqualTo(0);
         }
     }
 }
