e441df18113e5a39497d41cd75fb4baefb7777ce
leesangjin <tkdwls0628@gmail.com>
2023-07-26 15:52:51 +0900

    [Feat]: getPayload 다양한 케이스 테스트코드 추가

--- src/test/java/scs/planus/global/auth/service/JwtProviderTest.java
+++ src/test/java/scs/planus/global/auth/service/JwtProviderTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import scs.planus.global.auth.entity.Token;
+import scs.planus.global.exception.PlanusException;
 import scs.planus.support.ServiceTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static scs.planus.global.exception.CustomExceptionStatus.UNAUTHORIZED_ACCESS_TOKEN;
 
 @ServiceTest
 class JwtProviderTest {
@@ -14,7 +17,7 @@ class JwtProviderTest {
     private static final String SECRET_KEY = "A".repeat(64);
     private static final int ACCESS_TOKEN_EXPIRED_IN = 3600;
     private static final int REFRESH_TOKEN_EXPIRED_IN = 7200;
-    private static final String PAYLOAD = "planus";
+    private static final String PAYLOAD = "test@test";
 
     private final JwtProvider jwtProvider;
 
@@ -73,6 +76,34 @@ class JwtProviderTest {
 
         //then
         assertThat(payload).isEqualTo(PAYLOAD);
+    }
+
+    @DisplayName("AccessToken이 만료되었더라도 이를 파싱한다.")
+    @Test
+    void getPayload_If_Expired_Token(){
+        //given
+        JwtProvider expiredJwtProvider = new JwtProvider(SECRET_KEY, 0L, 0L);
+        Token token = expiredJwtProvider.generateToken(PAYLOAD);
+
+        //when
+        String payload = expiredJwtProvider.getPayload(token.getAccessToken());
+
+        //then
+        assertThat(payload).isEqualTo(PAYLOAD);
+    }
+
+    @DisplayName("AccessToken 형식이 잘못된 경우, 예외를 던진다")
+    @Test
+    void getPayload_Throw_Exception_If_Invalid_Token(){
+        //given
+        String invalidToken = "invalidToken";
+
+        //when
+        assertThatThrownBy(() ->
+                jwtProvider.getPayload(invalidToken))
+                .isInstanceOf(PlanusException.class)
+                .extracting("status")
+                .isEqualTo(UNAUTHORIZED_ACCESS_TOKEN);
     }
 
     @DisplayName("RefreshToken에는 payload가 존재하지 않는다.")
