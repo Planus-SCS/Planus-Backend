68c6b8a45dbd5433a39652003fae04858d8d2dfe
leesangjin <tkdwls0628@gmail.com>
2023-07-27 01:11:13 +0900

    [Feat]: todoCategoryId 추가

--- src/main/java/scs/planus/domain/category/dto/TodoCategoryForGroupResponseDto.java
+++ src/main/java/scs/planus/domain/category/dto/TodoCategoryForGroupResponseDto.java
@@ -9,11 +9,13 @@ import scs.planus.domain.category.entity.TodoCategory;
 @Builder
 public class TodoCategoryForGroupResponseDto {
 
+    private Long todoCategoryId;
     private String name;
     private Color color;
 
     public static TodoCategoryForGroupResponseDto of(TodoCategory todoCategory) {
         return TodoCategoryForGroupResponseDto.builder()
+                .todoCategoryId(todoCategory.getId())
                 .name(todoCategory.getName())
                 .color(todoCategory.getColor())
                 .build();
