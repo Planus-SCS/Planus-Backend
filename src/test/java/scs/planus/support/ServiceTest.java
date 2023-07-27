058c8f2138cd09ed62beaa2f593cb79b14779881
leesangjin <tkdwls0628@gmail.com>
2023-07-28 01:18:14 +0900

    [Refactor]: ServiceTest 어노테이션 대신 추상클래스로 변경

--- src/test/java/scs/planus/support/ServiceTest.java
+++ src/test/java/scs/planus/support/ServiceTest.java
@@ -5,15 +5,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import scs.planus.config.ExternalApiConfig;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
 @DataJpaTest
 @Import(ExternalApiConfig.class)
 @Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
-public @interface ServiceTest {
+public abstract class ServiceTest {
 }
