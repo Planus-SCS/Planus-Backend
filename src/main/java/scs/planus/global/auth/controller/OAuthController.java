f43db88dad87076b87a161d1f2b7429e68cb2204
lcj1204 <dlckdwo1204@naver.com>
2023-08-08 23:35:02 +0900

    [Feat] Apple 로그인 Controller 구현

--- src/main/java/scs/planus/global/auth/controller/OAuthController.java
+++ src/main/java/scs/planus/global/auth/controller/OAuthController.java
@@ -4,12 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import scs.planus.global.auth.dto.apple.AppleAuthRequestDto;
 import scs.planus.global.auth.dto.OAuthLoginResponseDto;
+import scs.planus.global.auth.service.apple.AppleOAuthService;
 import scs.planus.global.auth.service.OAuthService;
 import scs.planus.global.common.response.BaseResponse;
 
@@ -21,12 +19,22 @@ import scs.planus.global.common.response.BaseResponse;
 public class OAuthController {
 
     private final OAuthService oAuthService;
+    private final AppleOAuthService appleOAuthService;
 
     @GetMapping("/oauth/{provider}")
     @Operation(summary = "OAuth API")
     public BaseResponse<OAuthLoginResponseDto> socialLogin(@PathVariable String provider,
                                                            @RequestParam String code) {
         OAuthLoginResponseDto loginResponseDto = oAuthService.login(provider, code);
+        return new BaseResponse<>(loginResponseDto);
+    }
+
+    @PostMapping("/oauth/apple/login")
+    @Operation(summary = "Apple OAuth API")
+    public BaseResponse<OAuthLoginResponseDto> appleLogin(@RequestBody AppleAuthRequestDto appleAuthRequestDto) {
+
+        OAuthLoginResponseDto loginResponseDto = appleOAuthService.login(appleAuthRequestDto);
+
         return new BaseResponse<>(loginResponseDto);
     }
 }
