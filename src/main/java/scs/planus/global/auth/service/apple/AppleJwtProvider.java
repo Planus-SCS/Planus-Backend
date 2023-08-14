0a5c412e33a273c9c579be1058d6a4e73adfc701
lcj1204 <dlckdwo1204@naver.com>
2023-08-14 16:39:17 +0900

    [Refactor] .p8 파일 경로로 부터 ResourcesLoader 로 PrivateKey 생성하던 방식에서 @Value 로 주입받아 생성하는 코드로 수정

--- src/main/java/scs/planus/global/auth/service/apple/AppleJwtProvider.java
+++ src/main/java/scs/planus/global/auth/service/apple/AppleJwtProvider.java
@@ -2,31 +2,39 @@ package scs.planus.global.auth.service.apple;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import scs.planus.global.util.resourceLoader.ResourcesLoader;
+import scs.planus.global.exception.PlanusException;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Date;
+
+import static scs.planus.global.exception.CustomExceptionStatus.INTERNAL_SERVER_ERROR;
 
 @Component
 public class AppleJwtProvider {
     private final String clientId;
-    private final String clientSecret;
+    private final String clientSecretKey;
     private final String keyId;
     private final String teamId;
     private final String authUrl;
     private final long expiredIn;
 
     public AppleJwtProvider(@Value("${oauth.apple.client-id}") String clientId,
-                            @Value("${oauth.apple.client-secret.key-path}") String clientSecret,
+                            @Value("${oauth.apple.client-secret.key}") String clientSecretKey,
                             @Value("${oauth.apple.client-secret.expired-in}") long expiredIn,
-                            @Value("${oauth.apple.key-id}") String ketId,
+                            @Value("${oauth.apple.key-id}") String keyId,
                             @Value("${oauth.apple.team-id}") String teamId,
                             @Value("${oauth.apple.iss}") String authUrl) {
         this.clientId = clientId;
-        this.clientSecret = clientSecret;
+        this.clientSecretKey = clientSecretKey;
         this.expiredIn = expiredIn;
-        this.keyId = ketId;
+        this.keyId = keyId;
         this.teamId = teamId;
         this.authUrl = authUrl;
     }
@@ -43,7 +51,18 @@ public class AppleJwtProvider {
                 .setAudience(authUrl)
                 .setIssuedAt(now)
                 .setExpiration(expired)
-                .signWith(ResourcesLoader.createPrivateKey(clientSecret), SignatureAlgorithm.ES256)
+                .signWith(createPrivateKey(clientSecretKey), SignatureAlgorithm.ES256)
                 .compact();
+    }
+
+    private PrivateKey createPrivateKey(String clientSecretKey){
+        try {
+            byte[] encoded = Base64.decodeBase64(clientSecretKey);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            return keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new PlanusException(INTERNAL_SERVER_ERROR);
+        }
     }
 }
