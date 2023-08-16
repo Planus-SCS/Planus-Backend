f32cd60b16ee35dc0a80d544a8514e5cbd4e1118
lcj1204 <dlckdwo1204@naver.com>
2023-08-16 15:44:02 +0900

    [Feat] AppleAuthClient Test 코드 구현

---
+++ src/test/java/scs/planus/global/auth/service/apple/AppleAuthClientTest.java
@@ -0,0 +1,39 @@
+package scs.planus.global.auth.service.apple;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import scs.planus.global.auth.entity.apple.ApplePublicKey;
+import scs.planus.global.auth.entity.apple.ApplePublicKeys;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppleAuthClientTest {
+
+    private AppleAuthClient appleAuthClient;
+
+    public AppleAuthClientTest() {
+        this.appleAuthClient = new AppleAuthClient("https://appleid.apple.com/auth/keys");
+    }
+
+    @DisplayName("Apple Server 로 요청을 보내 PublicKey 를 응답 받아야 한다.")
+    @Test
+    void getApplePublicKey() {
+        // when
+        ApplePublicKeys applePublicKeys = appleAuthClient.getApplePublicKey();
+
+        boolean isRequestedKeysNonNull = applePublicKeys.getKeys().stream()
+                .allMatch(this::isAllNotNull);
+
+        // then
+        assertThat(applePublicKeys.getKeys()).hasSize(3);
+        assertThat(isRequestedKeysNonNull).isTrue();
+    }
+
+    private boolean isAllNotNull(ApplePublicKey applePublicKey) {
+        return Objects.nonNull(applePublicKey.getKty()) && Objects.nonNull(applePublicKey.getKid()) &&
+                Objects.nonNull(applePublicKey.getUse()) && Objects.nonNull(applePublicKey.getAlg()) &&
+                Objects.nonNull(applePublicKey.getN()) && Objects.nonNull(applePublicKey.getE());
+    }
+}
