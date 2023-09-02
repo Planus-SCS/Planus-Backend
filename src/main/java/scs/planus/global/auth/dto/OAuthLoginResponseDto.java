906d023b524bb926b9b8832193a946bb5248ee64
leesangjin <tkdwls0628@gmail.com>
2023-09-02 19:12:17 +0900

    [Feat]: 생성 메서드 구현

--- src/main/java/scs/planus/global/auth/dto/OAuthLoginResponseDto.java
+++ src/main/java/scs/planus/global/auth/dto/OAuthLoginResponseDto.java
@@ -2,6 +2,8 @@ package scs.planus.global.auth.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import scs.planus.domain.member.entity.Member;
+import scs.planus.global.auth.entity.Token;
 
 @Getter
 @Builder
@@ -10,4 +12,12 @@ public class OAuthLoginResponseDto {
     private Long memberId;
     private String accessToken;
     private String refreshToken;
+
+    public static OAuthLoginResponseDto of(Member member, Token token) {
+        return OAuthLoginResponseDto.builder()
+                .memberId(member.getId())
+                .accessToken(token.getAccessToken())
+                .refreshToken(token.getRefreshToken())
+                .build();
+    }
 }
