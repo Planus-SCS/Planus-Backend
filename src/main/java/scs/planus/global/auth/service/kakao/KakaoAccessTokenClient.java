1edea56fad0585085bbd060a681e73d41e6cddad
leesangjin <tkdwls0628@gmail.com>
2023-09-02 21:41:33 +0900

    [Feat]: KakaoAccessToken 받아오는 client 구현

---
+++ src/main/java/scs/planus/global/auth/service/kakao/KakaoAccessTokenClient.java
@@ -0,0 +1,55 @@
+package scs.planus.global.auth.service.kakao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+import scs.planus.global.auth.dto.OAuth2TokenResponseDto;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+@Component
+public class KakaoAccessTokenClient {
+
+    private final String kakaoTokenUri;
+    private final String kakaoClientId;
+    private final String kakaoClientSecret;
+    private final String redirectUri;
+
+    public KakaoAccessTokenClient(@Value("${oauth.kakao.token-uri}") final String kakaoTokenUri,
+                                  @Value("${oauth.kakao.client-id}") final String kakaoClientId,
+                                  @Value("${oauth.kakao.client-secret}") final String kakaoClientSecret,
+                                  @Value("${oauth.kakao.redirect-uri}") final String redirectUri) {
+        this.kakaoTokenUri = kakaoTokenUri;
+        this.kakaoClientId = kakaoClientId;
+        this.kakaoClientSecret = kakaoClientSecret;
+        this.redirectUri = redirectUri;
+    }
+
+    public OAuth2TokenResponseDto getToken(String code) {
+        return WebClient.create()
+                .post()
+                .uri(kakaoTokenUri)
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(accessTokenRequest(code))
+                .retrieve()
+                .bodyToMono(OAuth2TokenResponseDto.class)
+                .block();
+    }
+
+    private MultiValueMap<String, String> accessTokenRequest(String code) {
+        LinkedMultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("code", code);
+        data.add("client_id", kakaoClientId);
+        data.add("client_secret", kakaoClientSecret);
+        data.add("redirect_uri", redirectUri);
+        data.add("grant_type", "authorization_code");
+        return data;
+    }
+}
