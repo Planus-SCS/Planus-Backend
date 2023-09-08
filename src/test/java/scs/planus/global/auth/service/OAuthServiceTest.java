3513d394705df0454e21eaf52ba5cb2effce7a8c
leesangjin <tkdwls0628@gmail.com>
2023-09-08 22:43:42 +0900

    [Feat]: Kakao/Google 소셜 로그인 테스트코드 구현

--- src/test/java/scs/planus/global/auth/service/OAuthServiceTest.java
+++ src/test/java/scs/planus/global/auth/service/OAuthServiceTest.java
@@ -17,6 +17,8 @@ import scs.planus.global.auth.dto.apple.AppleClientSecretResponseDto;
 import scs.planus.global.auth.dto.apple.FullName;
 import scs.planus.global.auth.entity.Token;
 import scs.planus.global.auth.entity.userinfo.AppleUserInfo;
+import scs.planus.global.auth.entity.userinfo.GoogleUserInfo;
+import scs.planus.global.auth.entity.userinfo.KakaoUserInfo;
 import scs.planus.global.auth.entity.userinfo.OAuthUserInfo;
 import scs.planus.global.auth.service.apple.AppleJwtProvider;
 import scs.planus.global.auth.service.apple.AppleOAuthUserProvider;
@@ -28,6 +30,7 @@ import scs.planus.support.ServiceTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -38,6 +41,9 @@ import static scs.planus.global.exception.CustomExceptionStatus.INVALID_USER_NAM
 
 @Slf4j
 class OAuthServiceTest extends ServiceTest {
+
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    private static final String REFRESH_TOKEN = "REFRESH_TOKEN";
 
     private final MemberRepository memberRepository;
     @MockBean
@@ -56,6 +62,8 @@ class OAuthServiceTest extends ServiceTest {
     private final OAuthService oAuthService;
 
     private AppleAuthRequestDto appleAuthRequestDto;
+
+    private Token token;
 
     @Autowired
     public OAuthServiceTest(MemberRepository memberRepository,
@@ -82,6 +90,206 @@ class OAuthServiceTest extends ServiceTest {
                 appleOAuthUserProvider,
                 appleJwtProvider
         );
+    }
+
+    @BeforeEach
+    void init() {
+        token = Token.builder()
+                .accessToken(ACCESS_TOKEN)
+                .refreshToken(REFRESH_TOKEN)
+                .build();
+    }
+
+    @DisplayName("Kakao OAuth test")
+    @Nested
+    class KakaoOAuthTest {
+        private static final String CODE = "CODE";
+        private static final String EMAIL = "kakao@test.com";
+        private static final String NICKNAME = "KAKAO_USER";
+        private OAuthUserInfo kakaoUserInfo;
+
+        @BeforeEach
+        void init() {
+            KakaoUserInfo.KakaoAccount kakaoAccount = KakaoUserInfo.KakaoAccount.builder()
+                    .email(EMAIL)
+                    .profile(new KakaoUserInfo.KakaoAccount.Profile(NICKNAME))
+                    .build();
+
+            kakaoUserInfo = KakaoUserInfo.builder()
+                    .kakaoAccount(kakaoAccount)
+                    .build();
+        }
+
+        @DisplayName("카카오 소셜 로그인이 정상적으로 이루어져야 한다.")
+        @Test
+        void kakaoLogin() {
+            //given
+            given(kakaoOAuthUserProvider.getUserInfo(anyString()))
+                    .willReturn(kakaoUserInfo);
+            given(jwtProvider.generateToken(anyString())).willReturn(token);
+            willDoNothing().given(redisService).saveValue(anyString(), any(Token.class));
+
+            //when
+            OAuthLoginResponseDto oAuthLoginResponseDto = oAuthService.kakaoLogin(CODE);
+
+            //then
+            assertAll(
+                    () -> assertThat(oAuthLoginResponseDto.getMemberId()).isNotNull(),
+                    () -> assertThat(oAuthLoginResponseDto.getAccessToken()).isEqualTo(ACCESS_TOKEN),
+                    () -> assertThat(oAuthLoginResponseDto.getRefreshToken()).isEqualTo(REFRESH_TOKEN)
+            );
+            verify(kakaoOAuthUserProvider).getUserInfo(anyString());
+            verify(jwtProvider).generateToken(anyString());
+            verify(redisService).saveValue(anyString(), any(Token.class));
+        }
+
+        @DisplayName("카카오, 비활성화된 계정일 경우, 재활성화 및 사용자 정보 초기화가 되어야 한다.")
+        @Test
+        void kakaoLogin_saveOrGetExistedMember() {
+            // given
+            Member inActiveMember = memberRepository.save(
+                    Member.builder()
+                            .nickname("비활성계정")
+                            .email(EMAIL)
+                            .socialType(SocialType.KAKAO)
+                            .status(Status.INACTIVE)
+                            .build()
+            );
+
+            given(kakaoOAuthUserProvider.getUserInfo(anyString())).willReturn(kakaoUserInfo);
+            given(jwtProvider.generateToken(anyString())).willReturn(token);
+            willDoNothing().given(redisService).saveValue(anyString(), any(Token.class));
+
+            // when
+            OAuthLoginResponseDto oAuthLoginResponseDto = oAuthService.kakaoLogin(CODE);
+
+            // then
+            assertAll(
+                    () -> assertThat(oAuthLoginResponseDto.getMemberId()).isEqualTo(inActiveMember.getId()),
+                    () -> assertThat(inActiveMember.getNickname()).isEqualTo(NICKNAME),
+                    () -> assertThat(inActiveMember.getStatus()).isEqualTo(Status.ACTIVE)
+            );
+            verify(kakaoOAuthUserProvider).getUserInfo(anyString());
+            verify(jwtProvider).generateToken(anyString());
+            verify(redisService).saveValue(anyString(), any(Token.class));
+        }
+
+        @DisplayName("카카오, 이미 가입된 이메일인 경우, 예외가 발생한다.")
+        @Test
+        void kakaoLogin_Throw_Exception_If_Duplicated_Email() {
+            // given
+            Member member = memberRepository.save(
+                    Member.builder()
+                            .nickname("이미 존재하는계정")
+                            .email(EMAIL)
+                            .socialType(SocialType.GOOGLE)
+                            .status(Status.ACTIVE)
+                            .build()
+            );
+
+            given(kakaoOAuthUserProvider.getUserInfo(anyString())).willReturn(kakaoUserInfo);
+
+            // when & then
+            assertThatThrownBy(() ->
+                    oAuthService.kakaoLogin(CODE))
+                    .isInstanceOf(PlanusException.class)
+                    .extracting("status")
+                    .isEqualTo(ALREADY_EXIST_SOCIAL_ACCOUNT);
+        }
+    }
+
+    @DisplayName("Google OAuth test")
+    @Nested
+    class googleOAuthTest {
+        private static final String CODE = "CODE";
+        private static final String EMAIL = "google@test.com";
+        private static final String NICKNAME = "GOOGLE_USER";
+        private OAuthUserInfo googleUserInfo;
+
+        @BeforeEach
+        void init() {
+            googleUserInfo = GoogleUserInfo.builder()
+                    .email(EMAIL)
+                    .nickname(NICKNAME)
+                    .build();
+        }
+
+        @DisplayName("구글 소셜 로그인이 정상적으로 이루어져야 한다.")
+        @Test
+        void googleLogin() {
+            //given
+            given(googleOAuthUserProvider.getUserInfo(anyString()))
+                    .willReturn(googleUserInfo);
+            given(jwtProvider.generateToken(anyString())).willReturn(token);
+            willDoNothing().given(redisService).saveValue(anyString(), any(Token.class));
+
+            //when
+            OAuthLoginResponseDto oAuthLoginResponseDto = oAuthService.googleLogin(CODE);
+
+            //then
+            assertAll(
+                    () -> assertThat(oAuthLoginResponseDto.getMemberId()).isNotNull(),
+                    () -> assertThat(oAuthLoginResponseDto.getAccessToken()).isEqualTo(ACCESS_TOKEN),
+                    () -> assertThat(oAuthLoginResponseDto.getRefreshToken()).isEqualTo(REFRESH_TOKEN)
+            );
+            verify(googleOAuthUserProvider).getUserInfo(anyString());
+            verify(jwtProvider).generateToken(anyString());
+            verify(redisService).saveValue(anyString(), any(Token.class));
+        }
+
+        @DisplayName("구글, 비활성화된 계정일 경우, 재활성화 및 사용자 정보 초기화가 되어야 한다.")
+        @Test
+        void googleLogin_saveOrGetExistedMember() {
+            // given
+            Member inActiveMember = memberRepository.save(
+                    Member.builder()
+                            .nickname("비활성계정")
+                            .email(EMAIL)
+                            .socialType(SocialType.GOOGLE)
+                            .status(Status.INACTIVE)
+                            .build()
+            );
+
+            given(googleOAuthUserProvider.getUserInfo(anyString())).willReturn(googleUserInfo);
+            given(jwtProvider.generateToken(anyString())).willReturn(token);
+            willDoNothing().given(redisService).saveValue(anyString(), any(Token.class));
+
+            // when
+            OAuthLoginResponseDto oAuthLoginResponseDto = oAuthService.googleLogin(CODE);
+
+            // then
+            assertAll(
+                    () -> assertThat(oAuthLoginResponseDto.getMemberId()).isEqualTo(inActiveMember.getId()),
+                    () -> assertThat(inActiveMember.getNickname()).isEqualTo(NICKNAME),
+                    () -> assertThat(inActiveMember.getStatus()).isEqualTo(Status.ACTIVE)
+            );
+            verify(googleOAuthUserProvider).getUserInfo(anyString());
+            verify(jwtProvider).generateToken(anyString());
+            verify(redisService).saveValue(anyString(), any(Token.class));
+        }
+
+        @DisplayName("구글, 이미 가입된 이메일인 경우, 예외가 발생한다.")
+        @Test
+        void googleLogin_Throw_Exception_If_Duplicated_Email() {
+            // given
+            Member member = memberRepository.save(
+                    Member.builder()
+                            .nickname("이미 존재하는계정")
+                            .email(EMAIL)
+                            .socialType(SocialType.KAKAO)
+                            .status(Status.ACTIVE)
+                            .build()
+            );
+
+            given(googleOAuthUserProvider.getUserInfo(anyString())).willReturn(googleUserInfo);
+
+            // when & then
+            assertThatThrownBy(() ->
+                    oAuthService.googleLogin(CODE))
+                    .isInstanceOf(PlanusException.class)
+                    .extracting("status")
+                    .isEqualTo(ALREADY_EXIST_SOCIAL_ACCOUNT);
+        }
     }
 
     @DisplayName("Apple OAuth Test")
@@ -111,11 +319,6 @@ class OAuthServiceTest extends ServiceTest {
         @Test
         void login_Success_Join() {
             // given
-            Token token = Token.builder()
-                    .accessToken("accessToken")
-                    .refreshToken("refreshToken")
-                    .build();
-
             given(appleOAuthUserProvider.getUserInfo(anyString())).willReturn(appleUserInfo);
             given(jwtProvider.generateToken(anyString())).willReturn(token);
             willDoNothing().given(redisService).saveValue(anyString(), any(Token.class));
@@ -135,7 +338,7 @@ class OAuthServiceTest extends ServiceTest {
         @Test
         void login_Success_Reactivation() {
             // given
-            Member InactiveMember = memberRepository.save(
+            Member inActiveMember = memberRepository.save(
                     Member.builder()
                             .nickname("비활성계정")
                             .email(EMAIL)
@@ -143,11 +346,6 @@ class OAuthServiceTest extends ServiceTest {
                             .status(Status.INACTIVE)
                             .build()
             );
-
-            Token token = Token.builder()
-                    .accessToken("accessToken")
-                    .refreshToken("refreshToken")
-                    .build();
 
             given(appleOAuthUserProvider.getUserInfo(anyString())).willReturn(appleUserInfo);
             given(jwtProvider.generateToken(anyString())).willReturn(token);
@@ -157,7 +355,7 @@ class OAuthServiceTest extends ServiceTest {
             OAuthLoginResponseDto oAuthLoginResponseDto = oAuthService.appleLogin(appleAuthRequestDto);
 
             // then
-            assertThat(oAuthLoginResponseDto.getMemberId()).isEqualTo(InactiveMember.getId());
+            assertThat(oAuthLoginResponseDto.getMemberId()).isEqualTo(inActiveMember.getId());
             assertThat(oAuthLoginResponseDto).hasNoNullFieldsOrProperties();
 
             verify(appleOAuthUserProvider).getUserInfo(anyString());
