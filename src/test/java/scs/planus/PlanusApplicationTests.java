8fb0d020a28289323cc7218b6aabbf9c1ac64cb3
leesangjin <tkdwls0628@gmail.com>
2023-09-08 22:55:30 +0900

    [Refactor]: @SpringBootTest 주석 제거

--- src/test/java/scs/planus/PlanusApplicationTests.java
+++ src/test/java/scs/planus/PlanusApplicationTests.java
@@ -1,8 +1,9 @@
 package scs.planus;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class PlanusApplicationTests {
 
 	@Test
